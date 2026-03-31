ce48054630b76d8ad9b73cd9f4059bd2a604b9e3
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-03-31 09:31:20 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const API = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://smartnotes-backend-jf69.onrender.com", // Hardcode
   withCredentials: true,
 });
 
